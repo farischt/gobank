62c8d879610dbbbff2316ce612cd62f92385e60e
Faris CHTATOU <faris.manage@gmail.com>
2023-01-29 23:10:20 +0100

    add user and transaction

--- types.go
+++ types.go
@@ -4,25 +4,31 @@ import (
 	"time"
 )
 
-type CreateAccountDTO struct {
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-}
-
-type UpdateAccountDTO CreateAccountDTO
-
-type Account struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-
-	Balance   int64     `json:"balance" omitempty:"true"`
+/* ---------------------------------- User ---------------------------------- */
+type User struct {
+	ID        uint      `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at" omitempty:"true"`
 	UpdatedAt time.Time `json:"updated_at" omitempty:"true"`
 }
 
-type SecuredAccount struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+/* --------------------------------- Account -------------------------------- */
+type Account struct {
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"user_id"`
+	Balance   []uint8   `json:"balance" omitempty:"true"`
+	CreatedAt time.Time `json:"created_at" omitempty:"true"`
+	UpdatedAt time.Time `json:"updated_at" omitempty:"true"`
+}
+
+/* --------------------------------- Transaction ---------------------------- */
+type Transaction struct {
+	ID        uint      `json:"id"`
+	From      uint      `json:"from"`
+	To        uint      `json:"to"`
+	Amount    float64   `json:"amount"`
+	CreatedAt time.Time `json:"created_at" omitempty:"true"`
+	UpdatedAt time.Time `json:"updated_at" omitempty:"true"`
 }
